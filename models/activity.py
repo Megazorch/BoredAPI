40b3d2a4e8d43f9b805128624e6004211b8a5416
Roman Ivakhiv <megazorch@gmail.com>
2023-09-16 20:40:15 +0300

    Refactor after pylint check. Deleted useless function 'dict'.

--- models/activity.py
+++ models/activity.py
@@ -1,6 +1,7 @@
 """
 Activity instance
 """
+from typing import Optional
 
 
 class Activity:
@@ -12,19 +13,17 @@ class Activity:
     # activity_data: dict
     #     The activity data from the Bored API
 
-    def __init__(self, activity_data: dict):
+    def __init__(self, activity_data: Optional[dict] = None):
         self.activity_data = activity_data
         self.activity = ''
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Return the activity data as a string.
+        """
+        if self.activity_data is None:
+            return "No activity data"
+
         for key, value in self.activity_data.items():
             self.activity += f"{key}: {value} \n"
         return self.activity
-
-    def dict(self) -> dict:
-        """
-        Return the activity data as a dictionary.
-        :return activity_data: dict
-            The activity data as a dictionary.
-        """
-        return self.activity_data
