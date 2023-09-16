9c4b89ee4e7c6abd0feb3dbc5418b223cbdd8ec6
Roman Ivakhiv <megazorch@gmail.com>
2023-09-16 19:32:41 +0300

    Rewrote function 'to_tuple' into 'dict' for easier use in other functions

--- models/activity.py
+++ models/activity.py
@@ -21,12 +21,10 @@ class Activity:
             self.activity += f"{key}: {value} \n"
         return self.activity
 
-    def to_tuple(self):
-        return (self.activity_data['activity'],
-                self.activity_data['type'],
-                self.activity_data['participants'],
-                self.activity_data['price'],
-                self.activity_data['link'],
-                self.activity_data['key'],
-                self.activity_data['accessibility']
-                )
+    def dict(self) -> dict:
+        """
+        Return the activity data as a dictionary.
+        :return activity_data: dict
+            The activity data as a dictionary.
+        """
+        return self.activity_data
