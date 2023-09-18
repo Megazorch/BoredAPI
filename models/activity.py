646cff11dbd424ec06ae0875f211050fefe126a2
Roman Ivakhiv <megazorch@gmail.com>
2023-09-18 16:54:17 +0300

    Totally refactored class Activity so that it represents each value more explicitly

--- models/activity.py
+++ models/activity.py
@@ -8,22 +8,36 @@ class Activity:
     """
     Activity class
     """
-
-    # Constructor
-    # activity_data: dict
-    #     The activity data from the Bored API
-
-    def __init__(self, activity_data: Optional[dict] = None):
-        self.activity_data = activity_data
-        self.activity = ''
+    def __init__(self, activity_data: dict = None):
+        try:
+            self.activity = activity_data['activity']
+            self.activity_type = activity_data['type']
+            self.participants = activity_data['participants']
+            self.price = activity_data['price']
+            self.link = activity_data['link']
+            self.key = activity_data['key']
+            self.accessibility = activity_data['accessibility']
+        except KeyError or TypeError:
+            self.activity = None
+            self.activity_type = None
+            self.participants = None
+            self.price = None
+            self.link = None
+            self.key = None
+            self.accessibility = None
 
     def __str__(self) -> str:
         """
         Return the activity data as a string.
         """
-        if self.activity_data is None:
+        if self.activity is None:
             return "No activity data"
+        else:
+            return f"Activity: {self.activity}\n" \
+                   f"Type: {self.activity_type}\n" \
+                   f"Participants: {self.participants}\n" \
+                   f"Price: {self.price}\n" \
+                   f"Link: {self.link}\n" \
+                   f"Key: {self.key}\n" \
+                   f"Accessibility: {self.accessibility}\n"
 
-        for key, value in self.activity_data.items():
-            self.activity += f"{key}: {value} \n"
-        return self.activity
