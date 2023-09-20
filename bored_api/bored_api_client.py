0019f4553fff8735b094d00a18fb9935136f544e
Roman Ivakhiv <megazorch@gmail.com>
2023-09-20 10:49:16 +0300

    Implementing logging

--- bored_api/bored_api_client.py
+++ bored_api/bored_api_client.py
@@ -2,6 +2,7 @@
 Main functions for the API
 """
 import requests
+import logging
 
 
 class BoredApiClient:
@@ -18,13 +19,18 @@ class BoredApiClient:
         url = self.base_url + "activity"
 
         try:
+            logging.info("Fetching activity from Bored API")
+            logging.debug(f"Fetching activity from Bored API with parameters: {parameters}")
             response = requests.get(url, params=parameters)
 
             if response.status_code == 200:
                 activity_data = response.json()
+                logging.info(f"Fetched activity from Bored API: {activity_data}")
                 return activity_data
             else:
-                return {"error": f"Failed to fetch activity. Status code: {response.status_code}"}
+                logging.error(f"Failed to fetch activity. Status code: {response.status_code}")
+                logging.info("Finished")
 
         except Exception as e:
-            return {"error": f"An error occurred: {str(e)}"}
+            logging.error(f"Failed to fetch activity. Error: {str(e)}")
+            logging.info("Finished")
