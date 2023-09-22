de45ad9b1997f9c3bc861237a584ea2f34f1747e
Roman Ivakhiv <megazorch@gmail.com>
2023-09-22 15:45:43 +0300

    Add command `python main.py`

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 # Perform initialization of table 'activities' inside PostgreSQL database
 python main.py create-table
 
-# Start the main application process
-exec "$@"
+# Start the main application process with any additional arguments
+exec python main.py "$@"
