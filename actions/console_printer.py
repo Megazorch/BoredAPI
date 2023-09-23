e0d2f9d752589781193657274510e9fbd17d08cb
Roman Ivakhiv <megazorch@gmail.com>
2023-09-24 01:11:42 +0300

    `ConsolePrinter` now handles `Activity` objects

--- actions/console_printer.py
+++ actions/console_printer.py
@@ -4,6 +4,8 @@ Console output:
 import logging
 from prettytable import PrettyTable
 
+from database.activity import Activity
+
 logger = logging.getLogger("bored_api")
 
 
@@ -18,17 +20,21 @@ class ConsolePrinter:
 
         logger.info('Console printer initialized.')
 
-    def message_to_user(self, cursor_data: list[tuple]) -> None:
+    def message_to_user(self, cursor_data: Activity | list[Activity]) -> None:
         """
         Return the message to the user.
         """
-        for row in cursor_data:
-            row_as_list = list(row)
-            row_as_list[-1] = row_as_list[-1].strftime('%Y-%m-%d %H:%M:%S')     # row[-1] is the created_at column
-            self.console_table.add_row(row_as_list)
-
-        logger.info('Message to user printed.')
-        print(self.console_table)
+        if isinstance(cursor_data, Activity):
+            self.console_table.add_row(cursor_data.to_list())
+            logger.info('Message to user printed.')
+            print(self.console_table)
+        else:
+            for activity in cursor_data:
+                row_as_list = activity.to_list()
+                row_as_list[-1] = row_as_list[-1].strftime('%Y-%m-%d %H:%M:%S')     # row[-1] is the created_at column
+                self.console_table.add_row(row_as_list)
+            logger.info('Message to user printed.')
+            print(self.console_table)
 
     @staticmethod
     def error_response_from_api(error_message: dict) -> None:
